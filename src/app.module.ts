c27f935929ab82e81c6a556de667cdbfe899676d
zahariev <zaharievboyan@gmail.com>
2022-11-13 17:43:40 +0200

    add: config validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import Joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { AtGuard } from './auth/common/guards';
 import { PrismaModule } from './prisma/prisma.module';
@@ -17,6 +18,12 @@ import { PrismaModule } from './prisma/prisma.module';
     PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        AT_SECRET: Joi.string().required(),
+        RT_SECRET: Joi.string().required(),
+      }),
     }),
   ],
 })
